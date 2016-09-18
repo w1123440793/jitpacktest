929bfc430efc208345ce936778e72ed9ea1043db
wangchenchen <wangchenchen0225@163.com>
2016-09-18 17:15:10 +0800

    update

---
+++ mylibrary/src/main/java/jitpacktest/wang/cn/mylibrary/Test.java
@@ -0,0 +1,16 @@
+package jitpacktest.wang.cn.mylibrary;
+
+import android.util.Log;
+
+/**
+ * Author  wangchenchen
+ * CreateDate 2016/9/18.
+ * Email wcc@jusfoun.com
+ * Description
+ */
+public class Test {
+
+    public static void log(){
+        Log.e("jitpack","hahahahaha");
+    }
+}
