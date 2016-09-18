3e94cbaf9900d412dfd9d8b012817c42f8d5d00d
wangchenchen <wangchenchen0225@163.com>
2016-09-18 17:25:43 +0800

    updata library

--- app/src/main/java/cn/wang/jitpacktest/MainActivity.java
+++ app/src/main/java/cn/wang/jitpacktest/MainActivity.java
@@ -3,11 +3,14 @@ package cn.wang.jitpacktest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import jitpacktest.wang.cn.mylibrary.Test;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Test.log();
     }
 }
